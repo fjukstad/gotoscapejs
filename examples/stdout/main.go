008822514b9037be7e59ff790c7bd65a5930ab8e
Bjørn Fjukstad <bjorn@cs.uit.no>
2016-11-17 11:48:05 -0500

    more than 1 example

--- examples/stdout/main.go
+++ examples/stdout/main.go
@@ -10,39 +10,48 @@ func main() {
 	cy := &gotoscapejs.Cytoscape{}
 
 	cy.Add(gotoscapejs.Element{
+		Group: "nodes",
 		Data: gotoscapejs.Data{
 			Id: "a",
 		},
 	})
 
 	cy.Add(gotoscapejs.Element{
+		Group: "nodes",
 		Data: gotoscapejs.Data{
 			Id: "b",
 		},
 	})
 
 	cy.Add(gotoscapejs.Element{
+		Group: "nodes",
 		Data: gotoscapejs.Data{
 			Id: "c",
 		},
 	})
 
 	cy.Add(gotoscapejs.Element{
+		Group: "edges",
 		Data: gotoscapejs.Data{
+			Id:     "ab",
 			Source: "a",
 			Target: "b",
 		},
 	})
 
 	cy.Add(gotoscapejs.Element{
+		Group: "edges",
 		Data: gotoscapejs.Data{
+			Id:     "bc",
 			Source: "b",
 			Target: "c",
 		},
 	})
 
 	cy.Add(gotoscapejs.Element{
+		Group: "edges",
 		Data: gotoscapejs.Data{
+			Id:     "ca",
 			Source: "c",
 			Target: "a",
 		},
