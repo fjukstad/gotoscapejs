4ef02fa51333e245ed219fcc1a08039868b499bd
Bjørn Fjukstad <bjorn@cs.uit.no>
2016-11-17 15:35:18 -0500

    refactor

--- examples/web/main.go
+++ examples/web/main.go
@@ -10,65 +10,65 @@ import (
 
 func main() {
 
+	cy := &gotoscapejs.Cytoscape{}
+
+	cy.Add(gotoscapejs.Element{
+		Group: "nodes",
+		Data: gotoscapejs.Data{
+			Id: "a",
+		},
+	})
+
+	cy.Add(gotoscapejs.Element{
+		Group: "nodes",
+		Data: gotoscapejs.Data{
+			Id: "b",
+		},
+	})
+
+	cy.Add(gotoscapejs.Element{
+		Group: "nodes",
+		Data: gotoscapejs.Data{
+			Id: "c",
+		},
+	})
+
+	cy.Add(gotoscapejs.Element{
+		Group: "edges",
+		Data: gotoscapejs.Data{
+			Id:     "ab",
+			Source: "a",
+			Target: "b",
+		},
+	})
+
+	cy.Add(gotoscapejs.Element{
+		Group: "edges",
+		Data: gotoscapejs.Data{
+			Id:     "bc",
+			Source: "b",
+			Target: "c",
+		},
+	})
+
+	cy.Add(gotoscapejs.Element{
+		Group: "edges",
+		Data: gotoscapejs.Data{
+			Id:     "ca",
+			Source: "c",
+			Target: "a",
+		},
+	})
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/graph", func(w http.ResponseWriter, req *http.Request) {
-		cy := &gotoscapejs.Cytoscape{}
-
-		cy.Add(gotoscapejs.Element{
-			Group: "nodes",
-			Data: gotoscapejs.Data{
-				Id: "a",
-			},
-		})
-
-		cy.Add(gotoscapejs.Element{
-			Group: "nodes",
-			Data: gotoscapejs.Data{
-				Id: "b",
-			},
-		})
-
-		cy.Add(gotoscapejs.Element{
-			Group: "nodes",
-			Data: gotoscapejs.Data{
-				Id: "c",
-			},
-		})
-
-		cy.Add(gotoscapejs.Element{
-			Group: "edges",
-			Data: gotoscapejs.Data{
-				Id:     "ab",
-				Source: "a",
-				Target: "b",
-			},
-		})
-
-		cy.Add(gotoscapejs.Element{
-			Group: "edges",
-			Data: gotoscapejs.Data{
-				Id:     "bc",
-				Source: "b",
-				Target: "c",
-			},
-		})
-
-		cy.Add(gotoscapejs.Element{
-			Group: "edges",
-			Data: gotoscapejs.Data{
-				Id:     "ca",
-				Source: "c",
-				Target: "a",
-			},
-		})
-
 		cy.Write(w)
 	})
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		b, err := ioutil.ReadFile("index.html")
 		if err != nil {
-			http.Error(w, "NOT FOUND", 403)
+			http.Error(w, "NOT FOUND", 404)
 			return
 		}
 		w.Write(b)
